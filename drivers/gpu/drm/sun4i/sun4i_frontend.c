047a8fe27c02ebd918786f1db595b6ad52bf9efb
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2018-11-23 10:24:53 +0100

    drm/sun4i: frontend: Add support for the BGRX8888 input format
    This introduces support for the BGRX8888 input format for the frontend,
    with its associated pixel sequence value definition. Other fields are
    already configured correctly as they no longer depend on the format's
    fourcc directly.
    
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>

--- drivers/gpu/drm/sun4i/sun4i_frontend.c
+++ drivers/gpu/drm/sun4i/sun4i_frontend.c
@@ -127,6 +127,10 @@ static int sun4i_frontend_drm_format_to_input_mode(uint32_t fmt, u32 *val)
 static int sun4i_frontend_drm_format_to_input_sequence(uint32_t fmt, u32 *val)
 {
 	switch (fmt) {
+	case DRM_FORMAT_BGRX8888:
+		*val = SUN4I_FRONTEND_INPUT_FMT_DATA_PS_BGRX;
+		return 0;
+
 	case DRM_FORMAT_XRGB8888:
 		*val = SUN4I_FRONTEND_INPUT_FMT_DATA_PS_XRGB;
 		return 0;
@@ -149,6 +153,7 @@ static int sun4i_frontend_drm_format_to_output_fmt(uint32_t fmt, u32 *val)
 }
 
 static const uint32_t sun4i_frontend_formats[] = {
+	DRM_FORMAT_BGRX8888,
 	DRM_FORMAT_XRGB8888,
 };
 
